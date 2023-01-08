e1df6d14cb05abb70db5744e5a91901fce596c1a
huseynManaf <huseynim@code.edu.az>
2023-01-08 02:21:18 -0800

    init

---
+++ ConsoleApp61/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ConsoleApp61
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Korporativ informasiya sistemləri.
+
+1.İdarəetmə nədir.
+2.Optimal idarəetmə nəzəriyyəsindən istifadə edilməsi nəyi tələb edir.
+3.Planlaşdırma,Tənzimləmə.
+4.Uçot.
+5.Korporasiyanın strukturu.
+6.Korporasiyanın tipləri.
+7.Korporasiyanın modelləri
+8.Müssisənin idarəetmə sistemi.
+9.Təşkilati idarəetmə funksiyaları.
+10.Operativ,statistk mühasibat uçot haqqında .
+11.MR//ERP sistemi haqqında.
+12.İstehsalatın operativ idarə olunması.
+13.Proqnozlaşdırma.
+14.Layhələrin və proqramların  idarə edilməsi haqqında.
+15.Məhsulun təşkili haqqında .
+16.Maliyyənin idarə edilməsi.
+17.İqtisadi proseslərin proqnozlaşdırlması.
+18.İstehsalatın  planlaşdırlması və məhsul buraxılışının qrafikinin hazırlanması.
+19.Ehtiyatların idarə edilməsi.
+20.Resuslara tələbatın planlaşdırılması.
+21.İstehsalatın operativ idarə edilməsi.
+22.Material resuslarının və təchizatın idarə edilməsi.
+23.İdarəetmənin informasiya texnologiyaları.
+24.Texnoloji proseslərin mərkəzləşdirlmə
+dərəcəsi.
+25.Predmet sahəsi.
+26.Şəbəkənin qurulma üsulu.
+27.Müxtəlif tip təşkilatlarda və müssisələrdə informasiya texnologiyasının xüsusiyyətləri.
+28.Orta ölçülü müəsisə və təşkilatlar haqqında.
+29.İri müəssisələr(korporasiyalar haqqında).
+30.İT - nin köməyilə idarəetmə qərarlarının formalaşdırılması. 
+31.Qərar qəbuletmənin taktiki səviyyəsi.
+32.Qərar qəbuletmənin operativ səviyyəsi.
+33.İdarəetmənin informasiya sistemi.
+34.Avtomatlaşdırılmış informasiya sistemləri.
+35.İdarəetmə proseslərinin növünə görə informasiya sistemlərinin təsnifi haqqında.
+36.İdarəetmədə informasiya texnologiyalarının və sistemlərinin yaradılmasının metodik və təşkilati  əsasları haqqında.
+37.l - ci və ll-ci prinsip.
+38.lll - cü,lV - cü,V - ci prinsip.
+39.İT və İS -in yaradılmasının mərhələləri, metodları və təşkili haqqında.
+40.Layihə qabağı mərhələ.
+41.İşçi layihələndirmə.
+42.Avtomatlaşdırılmış layihələndirmə sistemləri.
+43.CASE texnologiyaları.
+44.Layhə inteqrasiyası.
+
+        }
+    }
+}
